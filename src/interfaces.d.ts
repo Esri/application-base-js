ec1220b0d95ddaf335b9d6ee1e022e3a3e3e61f3
Matt Driscoll <MDriscoll@esri.com>
2017-04-24 11:01:21 -0700

    add files

---
+++ src/interfaces.d.ts
@@ -0,0 +1,80 @@
+import Portal = require("esri/portal/Portal");
+import PortalItem = require("esri/portal/PortalItem");
+
+export interface ApplicationConfigs {
+  application?: ApplicationConfig;
+  config: ApplicationConfig;
+  local?: ApplicationConfig;
+  url?: ApplicationConfig;
+}
+
+export interface ApplicationConfig {
+  appid?: string;
+  basemapUrl?: string;
+  basemapReferenceUrl?: string;
+  center?: string;
+  components?: string;
+  embed?: boolean;
+  extent?: string;
+  find?: string;
+  group?: string | string[];
+  helperServices?: any;
+  level?: string;
+  marker?: string;
+  oauthappid?: string;
+  portalUrl?: string;
+  proxyUrl?: string;
+  title?: string;
+  viewpoint?: string;
+  webmap?: string | string[];
+  webscene?: string | string[];
+  [propName: string]: any;
+}
+
+export interface BoilerplateSettings {
+  environment: {
+    isEsri?: boolean;
+    webTierSecurity?: boolean;
+  };
+  localStorage?: {
+    fetch?: boolean;
+  };
+  group?: {
+    default?: string;
+    itemParams?: {
+      [propname: string]: any;
+    };
+    fetchItems?: boolean;
+    fetchInfo?: boolean;
+  };
+  portal?: {
+    fetch?: boolean;
+  };
+  urlParams?: string[];
+  webmap?: {
+    default?: string;
+    fetch?: boolean;
+  };
+  webscene?: {
+    default?: string;
+    fetch?: boolean;
+  }
+}
+
+export interface BoilerplateResult {
+  error?: Error;
+  value: any;
+  promise: IPromise<any>;
+}
+
+export interface BoilerplateResults {
+  applicationItem?: BoilerplateResult;
+  applicationData?: BoilerplateResult;
+  groupInfos?: BoilerplateResult[];
+  groupItems?: BoilerplateResult[];
+  localStorage?: ApplicationConfig;
+  portal?: Portal;
+  urlParams?: ApplicationConfig;
+  webMapItems?: BoilerplateResult[];
+  webSceneItems?: BoilerplateResult[];
+}

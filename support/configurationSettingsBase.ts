0532c7fde27296d4b33367e824cab984ffb61abc
Cameron Smith <cam11023@esri.com>
2020-11-13 14:17:39 -0800

    Accessor Subclassing

--- support/configurationSettingsBase.ts
+++ support/configurationSettingsBase.ts
@@ -1,53 +1,47 @@
-
-import { ApplicationConfig } from "../interfaces";
+import Accessor from 'esri/core/Accessor'
+import { property } from 'esri/core/accessorSupport/decorators'
+import { ApplicationConfig } from '../interfaces'
 
 /**
- * Base Class for all instant app's ConfigurationSettings classes. Handles 
+ * Base Class for all instant app's ConfigurationSettings classes. Handles
  * communication with the Config Panel for live updates during the configuration experience.
- * 
- * When extended in a template app, the app developer needs to add fields for all
- * configurable properties that existing for that particular app.
- * Ex:
- * 
- * 
- * 
  */
-class ConfigurationSettingsBase {
-
-    /** Determines if the App is being run within the Config Panel's IFrame */
+class ConfigurationSettingsBase extends Accessor {
+  /** Determines if the App is being run within the Config Panel's IFrame */
+  @property()
   withinConfigurationExperience: boolean =
-    window.location !== window.parent.location;
+    window.location !== window.parent.location
 
-  private _draft: ApplicationConfig = null;
-  private _draftMode: boolean = false;
+  private _draft: ApplicationConfig = null
+  private _draftMode: boolean = false
 
   constructor(params?: ApplicationConfig) {
-    this._draft = params?.draft;
-    this._draftMode = params?.mode === "draft";
+    super()
+    this._draft = params?.draft
+    this._draftMode = params?.mode === 'draft'
   }
 
   initialize() {
     if (this.withinConfigurationExperience || this._draftMode) {
       // Apply any draft properties
       if (this._draft) {
-        Object.assign(this, this._draft);
+        Object.assign(this, this._draft)
       }
 
       window.addEventListener(
-        "message",
+        'message',
         function (e) {
-          this._handleConfigurationUpdates(e);
+          this._handleConfigurationUpdates(e)
         }.bind(this),
-        false
-      );
+        false,
+      )
     }
   }
 
   _handleConfigurationUpdates(e) {
-    if (e?.data?.type === "cats-app") {
-      Object.assign(this, e.data);
+    if (e?.data?.type === 'cats-app') {
+      Object.assign(this, e.data)
     }
   }
-
 }
-export = ConfigurationSettingsBase;
+export = ConfigurationSettingsBase

5841e433658e587275f51c774de36b1223a0de7e
Cameron Smith <cam11023@esri.com>
2021-01-11 15:58:50 -0800

    Removed Files Removed configSettingsBase, basemapToggle, and widgetConfigUtils files for Templates Common Library Refactor

--- support/widgetConfigUtils/widgetConfigUtils.js
+++
@@ -1,9 +0,0 @@
-/**
- * This module contains common functions and interfaces to be used in different
- * widgetConfigUtil files.
- */
-define(["require", "exports"], function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-});
-//# sourceMappingURL=widgetConfigUtils.js.map
